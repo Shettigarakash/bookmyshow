f5d05126eda1d56a53145425dc0ebff10c8623b9
Akash Shettigar <akashshettigar97@gmail.com>
2022-04-27 10:55:34 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './header/search/search.component';
+import { MenuComponent } from './header/menu/menu.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    MenuComponent,
+    HomepageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
